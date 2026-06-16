8efea9979a9c0bac7f559ff8fb4f02168a9a0484
Birane LY <biranely94@gmail.com>
2026-06-16 09:21:01 +0000

    Création de la logique permettant à tous les composants enfants de pouvoir utiliser le mode clair ou sombre tout en évitant le Props Drilling.

---
+++ Next/src/context/darkModeContext.js
@@ -0,0 +1,21 @@
+import { createContext, useReducer } from "react";
+import {DarkModeReducer} from "./darkModeReducer"
+
+const INITIAL_STATE = {
+  darkMode: false,
+};
+
+// Création du Contexte
+export const DarkModeContext = createContext(INITIAL_STATE)
+
+//
+export const DarkModeContextProvider = ({children}) => {
+  const [state, dispatch] = useReducer(DarkModeReducer, INITIAL_STATE);
+  return(
+  <DarkModeContext.Provider value= {{darkMode: state.darkMode,dispatch}}>
+    {children} // Tous les composants enfants
+  </DarkModeContext.Provider>
+  )
+
+}
+
