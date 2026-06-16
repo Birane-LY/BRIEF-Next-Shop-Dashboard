8cff84adf0cb3834fa1c5d65a5de6b412e78d885
Adji Aissatou Wade SAMBE <adjiaissatouwade@gmail.com>
2026-06-16 10:04:16 +0000

    Configuration de la page Home

--- Next/src/pages/home/Home.jsx
+++ Next/src/pages/home/Home.jsx
@@ -1,1 +1,13 @@
 import Sidebar from "../../components/Sidebar/Sidebar";
+import NavBar from "../../components/NavBar/NavBar";
+
+function Home(){
+    return(
+        <div classname="Home">
+            <NavBar/>
+            <Sidebar/>
+        </div>
+    )
+}
+
+export default Home
