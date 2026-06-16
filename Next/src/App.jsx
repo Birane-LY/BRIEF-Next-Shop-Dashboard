b6fc6cf70817572b21e937c1a95a2ca222134c43
Birane LY <biranely94@gmail.com>
2026-06-16 10:32:30 +0000

    Configuration des routes de nos composants.

--- Next/src/App.jsx
+++ Next/src/App.jsx
@@ -1,13 +1,26 @@
 
-import './App.css'
+import "./style/dark.scss";
+import {BrowserRouter,Routes, Route,Navigate} from 'react-router-dom';
+import { useContext } from 'react';
+import Home from './pages/Home';
+import List from './pages/List';
+import DarkModeContext from './context/darkModeContex'
 
 function App() {
-  
-
+  const {darkMode} = useContext(DarkModeContext)
   return (
-    <>
-     
-    </>
+    <div className={darkMode ? "app dark" : "app"}>
+      <BrowserRouter>
+        <Routes>
+          <Route>
+            <Home/>
+          </Route>
+          <Route>
+            <List/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
