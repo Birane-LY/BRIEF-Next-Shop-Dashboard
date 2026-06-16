1b17f2b8bd59f4c19de8031a9afd431d73891917
Birane LY <biranely94@gmail.com>
2026-06-16 13:03:40 +0000

    Création du Mock et Affichage des produits.

---
+++ Next/src/FormSource.jsx
@@ -0,0 +1,92 @@
+// Temporary Data Source
+
+export const userInputs = [
+
+{ 
+id: "username",
+label: "Prénom",
+type: "text",
+placeholder: "Birane "
+},
+
+{ 
+id: "displayName",
+label: "Nom",
+type: "text",
+placeholder: "LY"
+},
+
+{ 
+id: "email",
+label: "Email",
+type: "email",
+placeholder: "birane@gmail.com"
+},
+
+{ 
+id: "phone",
+label: "Téléphone",
+type: "text",
+placeholder: "77 123 45 67"
+},
+
+{ 
+id: "password",
+label: "Mot de passe",
+type: "password",
+},
+
+{ 
+id: "adress",
+label: "Adresse",
+type: "text",
+placeholder: "Dakar"
+},
+
+{ 
+id: "country",
+label: "Pays",
+type: "text",
+placeholder: "Sénégal"
+},
+
+];
+
+export const productInputs = [
+
+{
+id: 1,
+label: "Titre",
+type: "text",
+placeholder: "Apple MacBook Pro",
+},
+
+{
+id: 2,
+label: "Description",
+type: "text",
+placeholder: "Description",
+},
+
+{
+id: 3,
+label: "Catégorie",
+type: "text",
+placeholder: "Computer",
+},
+
+{
+id: 4,
+label: "Prix",
+type: "text",
+placeholder: "100",
+},
+
+{
+id: 5,
+label: "Stock",
+type: "text",
+placeholder: "En stock",
+},
+
+];
--- Next/src/pages/home/Home.jsx
+++ Next/src/pages/home/Home.jsx
@@ -1,13 +1,24 @@
+import React from 'react'
 import Sidebar from "../../components/Sidebar/Sidebar";
 import NavBar from "../../components/NavBar/NavBar";
+import Table from "../../table/Table";
 
-function Home(){
-    return(
-        <div className="Home">
-            <NavBar/>
-            <Sidebar/>
-        </div>
-    )
+import "./home.scss"
+
+const Home = () => {
+  return (
+    <div className="home">
+    <Sidebar />
+    <div className="homeContainer">
+      <NavBar/>
+      <div className="listContainer">
+     <div className="listTitle">Dernières transactions</div>
+     <Table />
+      </div>
+    </div>
+    
+    </div>
+  )
 }
 
 export default Home
