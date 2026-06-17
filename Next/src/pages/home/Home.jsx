9e20fe6b1bbaeeaf67f0148bcef65ea2b172975d
Adji Aissatou Wade SAMBE <adjiaissatouwade@gmail.com>
2026-06-17 12:57:25 +0000

    Passage du context au page Home.jsx

--- Next/src/pages/home/Home.jsx
+++ Next/src/pages/home/Home.jsx
@@ -2,8 +2,11 @@ import Layout from "../../components/Layout/Layout";
 import Table from "../../table/Table"; 
 import Products from "../../table/ProductTable"; 
 import "./home.scss";
+import { useProducts } from '../../context/ProductContext'; // ← Importez le hook
 
 const Home = () => {
+  const { produits } = useProducts(); // ← Récupérez les produits du Context
+
   return (
     <Layout>
       <div className="listContainer">
@@ -11,9 +14,10 @@ const Home = () => {
         <Table />
       </div>
       <div className="listTitle">Stocks actuels</div>
-      <Products />
+      <Products produits={produits} /> {/* ← Passez les produits en prop */}
     </Layout>
   );
 };
 
 export default Home;
+
